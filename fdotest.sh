94bf3e6eeaed46c026034e8232f9e4a8ce1e8223
Sven Dowideit <SvenDowideit@home.org.au>
2021-12-10 10:36:16 +1000

    ah, that works
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- fdotest.sh
+++ fdotest.sh
@@ -10,7 +10,8 @@ PORTAINER_NAME="p1"
 USERNAME="admin"
 PWD='59QxqYiE5yWJb064'
 DEVICESERIALNUMBER=$(uuidgen | sed 's/-.*//')
-MANUFACTURER=http://localhst:8039/
+MANUFACTURER=http://10.13.13.10:8039/
+OWNER=http://10.13.13.10:8042/
 
 echo "device serial number: ${DEVICESERIALNUMBER}"
 docker volume create "fdo-device-${DEVICESERIALNUMBER}"
@@ -36,7 +37,7 @@ echo "portainer login"
 jwt=`http --verify false --ignore-stdin ${PORTAINER_URL}/api/auth username="${USERNAME}" password="${PWD}" | jq .jwt | cut -d '"' -f 2`
 echo "jwt = ${jwt}"
 
-VOUCHERS=$(http --verify false --form --ignore-stdin GET ${PORTAINER_URL}/api/hosts/fdo/list "Authorization: ${jwt}")
+VOUCHERS=$(http --verify false --form --ignore-stdin GET ${PORTAINER_URL}/api/fdo/list "Authorization: ${jwt}")
 echo "GetVouchers: ${VOUCHERS}"
 
 echo "post voucher"
@@ -44,17 +45,17 @@ TMPFILE=$(mktemp)
 http --verify false --verbose --debug \
     -d --output ${TMPFILE} \
     --form --ignore-stdin POST \
-    ${PORTAINER_URL}/api/hosts/fdo/register \
+    ${PORTAINER_URL}/api/fdo/register \
     "Authorization: ${jwt}" \
     voucher@${DEVICESERIALNUMBER}.bin
 
 SERVICEINFO=$(cat ${TMPFILE})
-GUID=$(cat ${TMPFILE} | sed 's/.*guid = //' | sed 's/\\n.*//')
+GUID=$(cat ${TMPFILE} | jq -r .guid )
 rm ${TMPFILE}
 
 echo "PutVouchers: ${SERVICEINFO}"
 
-VOUCHERS=$(http --verify false --form --ignore-stdin GET ${PORTAINER_URL}/api/hosts/fdo/list "Authorization: ${jwt}")
+VOUCHERS=$(http --verify false --form --ignore-stdin GET ${PORTAINER_URL}/api/fdo/list "Authorization: ${jwt}")
 # this list should container the device GUID 
 echo "GetVouchers: ${VOUCHERS}"
 
@@ -63,6 +64,47 @@ echo
 echo "Waiting for 60 seconds, for Manufacturer to tell RZ about 'TO0 completed for GUID: ${GUID}' "
 echo
 sleep 60
+
+# add more fdo_sys resources
+# see https://github.com/secure-device-onboard/pri-fidoiot/blob/2bac84514d02c89583585d3dfaa305681625c82a/component-samples/demo/owner/README.md
+# https://secure-device-onboard.github.io/docs/0.5.0/fdo/fdo-serviceinfo-sys/
+#curl -v --digest -X PUT --user apiUser:05EV9CbHbAQANc1t -H "Content-Type: application/octet-stream" --data 1 'http://10.13.13.10:8042/api/v1/device/svi?module=fdo_sys&var=active&priority=12&guid='${GUID}
+#curl -v --digest -X PUT --user apiUser:05EV9CbHbAQANc1t -H "Content-Type: application/octet-stream" --data "touch /sven" 'http://10.13.13.10:8042/api/v1/device/svi?module=fdo_sys&var=exec&priority=12&guid='${GUID}
+#curl -v --digest -X PUT --user apiUser:05EV9CbHbAQANc1t -H "Content-Type: application/octet-stream" --data "touch /sven" 'http://10.13.13.10:8042/api/v1/device/svi?module=fdo_sys&var=exec&priority=12&device=alldevice&os=allos&arch=X86_64&hash=allhash&guid='${GUID}
+#curl -v --digest -X PUT --user apiUser:05EV9CbHbAQANc1t -H "Content-Type: application/octet-stream" --data "true" 'http://10.13.13.10:8042/api/v1/device/svi?module=fdo_sys&var=active&priority=12&device=alldevice&os=allos&arch=X86_64&hash=allhash&guid='${GUID}
+
+#curl -v --digest -X PUT --user apiUser:05EV9CbHbAQANc1t -H "Content-Type: application/octet-stream" --data "touch /sven" 'http://10.13.13.10:8042/api/v1/device/svi?module=fdo_sys&var=exec&priority=12&arch=X86_64&hash=allhash&guid='${GUID}
+
+#curl -v --digest -X PUT --user apiUser:05EV9CbHbAQANc1t -H "Content-Type: application/octet-stream" --data "touch /sven2" 'http://10.13.13.10:8042/api/v1/device/svi?module=fdo_sys&var=exec&priority=12&device=Intel-FDO-Linux&arch=x86&os=Linux&version=Ubuntu-14&hash=allhash&guid='${GUID}
+
+# from https://github.com/secure-device-onboard/test-fidoiot/blob/6bdf0772a659ee997d4ee212f47ca65afc1af0b2/priTests/src/test/java/org/fidoalliance.fdo/test/PriSmokeTest.java
+# use priority to ensure they get sent to the device in the right order.
+# enable fdo_sys
+curl -v --digest -X PUT --user apiUser:05EV9CbHbAQANc1t \
+    -H "Content-Type: application/octet-stream" \
+    "${OWNER}api/v1/device/svi?module=fdo_sys&var=active&bytes=F5&priority=0&guid=${GUID}"
+# send the file
+curl -v --digest -X PUT --user apiUser:05EV9CbHbAQANc1t \
+    -H "Content-Type: application/octet-stream" --data-binary '@sven.sh' \
+    "${OWNER}api/v1/device/svi?module=fdo_sys&var=filedesc&priority=1&filename=init-${DEVICESERIALNUMBER}.sh&guid=${GUID}"
+# run the file (this needs to be in cbor...)
+#curl -v --digest -X PUT --user apiUser:05EV9CbHbAQANc1t \
+#    -H "Content-Type: application/octet-stream" --data-binary '@sven.sh' \
+#    "${OWNER}api/v1/device/svi?module=fdo_sys&var=exec&priority=2&filename=init-${DEVICESERIALNUMBER}.sh&guid=${GUID}"
+
+# curl -v --digest -X PUT --user apiUser:05EV9CbHbAQANc1t \
+#     -H "Content-Type: application/octet-stream" --data-binary '@sven.sh' \
+#     'http://10.13.13.10:8042/api/v1/device/svi?module=fdo_sys&var=filedesc&priority=1&filename=init-${DEVICESERIALNUMBER}.sh&guid='${GUID}
+
+# curl --location --digest -u apiUser:" + ownerApiPass + " "
+#               + "--request PUT 'http://localhost:8042/api/v1/device/svi?module=fdo_sys&"
+#               + "var=filedesc&priority=1&filename=linux64.sh&guid=" + guid
+#               + "' --header 'Content-Type: application/octet-stream' --data-binary "
+#               + "'@common/src/main/resources/linux64.sh'
+
+# list them
+curl -v --digest -X GET --user apiUser:05EV9CbHbAQANc1t http://10.13.13.10:8042/api/v1/device/svi?guid=${GUID}
+
 
 echo to complete the FDO onboarding, please run:
 echo
